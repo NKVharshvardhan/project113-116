1217ffb8e605fceae38a6c8d8d610bd04830c366
NKVharshvardhan <73694587+NKVharshvardhan@users.noreply.github.com>
2021-05-21 18:03:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload(){
+
+}
+
+
+function setup(){
+    var canvas=createCanvas(300,300);
+canvas.center();
+}
+
+function draw(){
+   
+    }
+
+    function take_snapshot(){
+        save("123456789.png");
+    }
+    
